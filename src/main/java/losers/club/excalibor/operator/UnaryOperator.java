539d3fa71b53219bf1edd72bf3f17ffbaa037944
Anastasia Miles <anastarm@uci.edu>
2018-04-24 11:55:16 -0700

    Repair unary operator to handle one vs two arguments

--- src/main/java/losers/club/excalibor/operator/UnaryOperator.java
+++ src/main/java/losers/club/excalibor/operator/UnaryOperator.java
@@ -5,9 +5,12 @@ import losers.club.excalibor.argument.Argument;
 public abstract class UnaryOperator extends Operator {
   @Override
   public Argument evaluate(Argument lhs, Argument rhs) {
-    // Ignores lhs argument (should be null?)
+    if (lhs != null) {
+      throw new IllegalArgumentException(String.format("Only one argument allowed for operation %s",
+          this.getSymbol()));
+    }
     return evaluate(rhs);
   }
-  
+
   public abstract Argument evaluate(Argument rhs);
 }
